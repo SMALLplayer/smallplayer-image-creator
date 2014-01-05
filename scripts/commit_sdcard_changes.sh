30635425461fa71926b2203db0af6b13e889ea8e
Rico van Genugten <rico.van.genugten@gmail.com>
2014-01-05 15:34:02 +0100

    initial commit of commit_sdcard_changes.sh

---
+++ scripts/commit_sdcard_changes.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+  echo "Please execute with sudo or as root"
+  echo " example: sudo ./commit_sdcard_changes.sh /dev/sdb"
+
+  exit 1
+fi
+
+if [ -z "$1" ]; then
+  echo "Please execute with your drive as option"
+  echo " example: sudo ./commit_sdcard_changes.sh /dev/sdb"
+  exit 1
+fi
+
+STOR_PART="${1}2"
+STOR_PART_MNT="/tmp/sp_stor_part"
+STOR_PART_REPO="../storage"
+RSYNC="rsync -a --delete-after"
+
+echo "Mounting storage partition from sd-card"
+umount $STOR_PART
+rm -rf $STOR_PART_MNT
+mkdir -p $STOR_PART_MNT
+mount $STOR_PART $STOR_PART_MNT
+
+echo "Synchronizing storage folders from sd-card to repo"
+$RSYNC $STOR_PART_MNT/.xbmc     $STOR_PART_REPO
+$RSYNC $STOR_PART_MNT/tvshows   $STOR_PART_REPO
+$RSYNC $STOR_PART_MNT/videos    $STOR_PART_REPO
+$RSYNC $STOR_PART_MNT/music     $STOR_PART_REPO
+$RSYNC $STOR_PART_MNT/pictures  $STOR_PART_REPO
+$RSYNC $STOR_PART_MNT/downloads $STOR_PART_REPO
+
+echo "Reverting changes to oe_settings.xml"
+git checkout -- $STOR_PART_REPO/.xbmc/userdata/addon_data/service.openelec.settings/oe_settings.xml
+
+echo "Unmounting and syncing"
+sync
+umount $STOR_PART
+
+echo "Done"
+
